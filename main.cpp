e37c750f3148746221f39e8652983b13245fe182
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-09-30 11:23:16 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <stdio.h>
+
+// テンプレ
+template <typename T>
+T Min(T value1, T value2){ return value1 < value2 ? value1 : value2; }
+
+// charはテンプレ解除
+template<>
+char Min<char>(char value1, char value2){ 
+	printf("char型は使用できません。\n"); 
+	return 0;
+}
+
 int main(){ //開く
 
-	printf("Hello world.\n");
-	printf("こんにちは\n");
+	// 出力
+	printf("Min<int>(114,514) = %d\n", Min<int>(114, 514));
+	printf("Min<float>(11.4f,51.4f) = %f\n", Min<float>(11.4f, 51.4f));
+	printf("Min<double>(1919.0,810.0) = %lf\n", Min<double>(1919.0, 810.0));
+	printf("Min<char>(a,b) = %c\n", Min<char>('a', 'b'));
 
 	return 0;
 } //閉じる
