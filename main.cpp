d98276cf908fbdc40514de44645a4dc9e520fc5d
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-09-30 14:27:28 +0900

    01_03

--- main.cpp
+++ main.cpp
@@ -1,63 +1,114 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
 #include <cstdint>
+#include <random>
+#include <chrono>
 
-// 最低賃金
-const int32_t  minimunSalary = 1072;
 
-// 給料形式の名前のenum
-enum class SalalyType : int32_t{
-	NORMAL = 0,
-	RECURSIVE
-};
+/////////////////////////////////////////////////
+// 乱数生成
+/////////////////////////////////////////////////
 
-// 再帰計算関数
-int32_t CalcRecursiveSalary(int32_t loop, int32_t kMaxLoop, int32_t currentSalary){
+int32_t Random(int min, int max){
+	// 乱数生成器を作成
+	std::random_device rd;
+	std::mt19937 gen(rd());
 
-	int32_t salary = currentSalary * 2 - 50;
-	if(loop + 1 >= kMaxLoop){
-		return salary;
-	} else{
-		return CalcRecursiveSalary(loop + 1, kMaxLoop, salary);
+	// minからmaxまでの一様分布を設定
+	std::uniform_int_distribution<> distrib(min, max);
+
+	// 乱数を生成して返す
+	return distrib(gen);
+}
+
+
+/////////////////////////////////////////////////
+// 結果の回答関数
+/////////////////////////////////////////////////
+
+void Correct(){
+	printf("正解！\n\n");
+}
+
+void Icorrect(){
+	printf("不正解...\n\n");
+}
+
+typedef void(*pFunc)();
+
+
+/////////////////////////////////////////////////
+// 時限発動
+/////////////////////////////////////////////////
+
+void TimedFunc(float sec, pFunc func){
+
+	// エントリー時の時間
+	std::chrono::steady_clock::time_point enterTime_ = std::chrono::high_resolution_clock::now();
+
+	while(true){
+		// 現在の時間との差分を取る
+		std::chrono::steady_clock::time_point currentTime_ = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<float> totalTime = currentTime_ - enterTime_;
+
+		// 指定時間が経過したら
+		if(totalTime.count() >= sec){
+			(*func)();//発動
+			break;
+		}
 	}
-};
-
-// 給料計算関数
-int32_t CalcSalary(SalalyType salaryType, int32_t hours){
-
-	int32_t salary = 0;
-
-	switch(salaryType){
-
-	case SalalyType::NORMAL:
-		salary = minimunSalary * hours;
-		return salary;
-		break;
-
-	case SalalyType::RECURSIVE:
-		return CalcRecursiveSalary(0, hours, 100);
-		break;
-
-	default:
-		return 0;
-		break;
-
-	}
-};
+}
 
 
-
+/////////////////////////////////////////////////
+// main
+/////////////////////////////////////////////////
 int main(){ //開く
 
-	// 通常形式の計算12時間分
-	for(int32_t i = 1; i <= 12; i++){
-		printf("NORMAL: %d 時間 : %d 円\n", i, CalcSalary(SalalyType::NORMAL, i));
-	}
+	// 関数ポインタ
+	pFunc func;
+	float waitTime = 3.0f;
 
-	printf("\n");
+	//
+	while(true){
 
-	// 再帰形式の計算12時間分
-	for(int32_t i = 1; i <= 12; i++){
-		printf("RECURSIVE: %d 時間 : %d 円\n", i, CalcSalary(SalalyType::RECURSIVE, i));
+		// 出題
+		printf("乱数当てゲーム！\n\n");
+		printf("これから抽選される乱数は偶数？奇数？\n");
+		printf("0.偶数   1.奇数\n\n");
+
+		// 乱数取得
+		uint32_t result = Random(1, 100);
+
+		// 入力受付
+		uint32_t input;
+
+		while(true){
+
+			// 入力受付
+			printf("answer: ");
+			std::cin >> input;
+
+			if(input > 1){// 範囲外の場合続行
+				printf("\n範囲外です！(0 or 1)\n\n");
+
+			} else{// 回答
+
+				printf("結果は...");
+
+				if(result % 2 == input){
+					func = &Correct;
+					TimedFunc(waitTime, func);
+
+				} else{
+					func = &Icorrect;
+					TimedFunc(waitTime, func);
+				}
+
+				break;
+			}
+		}
 	}
 
 	return 0;
